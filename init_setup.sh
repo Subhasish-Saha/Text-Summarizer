5c02c6d3273679e0028b542424b044887eed7a86
Subhasish <subhasishsaha007@gmail.com>
2024-05-08 23:12:37 +0530

    added env_text

--- init_setup.sh
+++ init_setup.sh
@@ -1,10 +1,10 @@
 echo [$(date)]: 'START'
 echo [$(date)]: 'Creating conda env with python 3.8'
-conda create --prefix ./textSummarizer python=3.8 -y
-echo [$(date)]: 'activate textSummarizer'
-source activate ./textSummarizer
+conda create --prefix ./env_text python=3.8 -y
+echo [$(date)]: 'activate env_text'
+source activate ./env_text
 echo [$(date)]: 'installing requirements'
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 echo [$(date)]: 'Setup END'
 
 #RUN : bash init_setup.sh
