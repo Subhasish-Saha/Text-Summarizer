e9b1fb8a0490483b97839acf52f711c54311aa64
Subhasish Saha <subhasishsaha007@gmail.com>
2024-05-10 06:34:43 +0530

    Added :  Data Ingestion Pipeline

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info(f'Subhasish')
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f'>>>>>>> Stage : {STAGE_NAME} started <<<<<<<')
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f'>>>>>>> Stage : {STAGE_NAME} Completed <<<<<<< \n\nx============x')
+except Exception as e:
+    logger.exception(e)
+    raise e
